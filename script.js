c4de6160a76753ec0ce1c59998644dc08a8953ed
charlaine1203 <charlaine680@gmail.com>
2025-06-04 13:13:37 +0100

    script.js

--- script.js
+++ script.js
@@ -43,4 +43,18 @@ document.addEventListener('DOMContentLoaded',() =>
             message.style.color="red";
         }
     });
+    form.addEventListener('DOMContentLoaded', function() {
+    const togglePassword = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('password');
+
+    togglePassword.addEventListener('click', function() {
+            const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordInput.setAttribute('type', type);
+            if (type === 'password') {
+                this.textContent = '👁'; 
+            } else {
+                this.textContent = '🙈'; 
+            }
+        });
+    });
 });
